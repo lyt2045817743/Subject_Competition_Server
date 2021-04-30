9303c11d0fa6ef585237c3975561ca33eb3891a4
lyt2045817743 <2045817743@qq.com>
2021-04-30 19:52:41 +0800

    查询用户关注列表功能

--- routes/userContact/userContact.js
+++ routes/userContact/userContact.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')();
 const UserContact = require('../../models/userContact');
 const initCtx = require('../../util/initCtx');
+const User = require('../../models/user')
 
 router.post('/followSomeone', async ctx => {
     const { fromUser, toUser, status } = ctx.request.body;
@@ -24,6 +25,32 @@ router.post('/followSomeone', async ctx => {
 
     await contact.save();
     new initCtx(ctx, '关注成功', []).success()
+})
+
+router.get('/getFollow', async ctx => {
+    const { pageNum, pageCount, keyword, numberId } = ctx.query;
+
+    let listdata = [];
+
+    if(pageNum == -1 && pageCount == -1) {
+        listdata = await UserContact.find({"fromUser": numberId})
+        .sort({'createTime': -1})
+
+    } else {
+        listdata = await UserContact.find({"fromUser": numberId})
+        .sort({'createTime': -1})
+        .skip((pageCount-1)*pageNum)
+        .limit(Number(pageNum));
+    }
+
+    let userList = []
+
+    for(let i = 0; i < listdata.length; i++) {
+        const result = await User.findOne({ numberId: listdata[i].toUser });
+        userList.push(result)
+    }
+
+    new initCtx(ctx, 'SUCCESS', userList).success();
 })
 
 module.exports = router.routes()
